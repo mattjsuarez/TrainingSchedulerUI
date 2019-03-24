7f7365b7ac0213f1cd2da5947cebcacc40452fc6
mattsuarez <matt.suarez@gmail.com>
2019-03-24 16:19:15 -0400

    goal banner menu bar schedule tab has html str period

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { AppRoutingModule } from "./app-routing/app-routing.module";
+import { ConfigPageComponent } from "./config-page/config-page.component";
+import { ContactPageComponent } from "./contact-page/contact-page.component";
+
+import { GoalBannerComponent } from "./goal-banner/goal-banner.component";
+import { GoalsPageComponent } from "./goals-page/goals-page.component";
+import { HomePageComponent } from "./home-page/home-page.component";
+import { ProblemPyramidPageComponent } from "./problem-pyramid-page/problem-pyramid-page.component";
+import { SchedulePageComponent } from "./schedule-page/schedule-page.component";
+import { TopNavBarComponent } from "./top-nav-bar/top-nav-bar.component";
+
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConfigPageComponent,
+    ContactPageComponent,
+    GoalBannerComponent,
+    GoalsPageComponent,
+    HomePageComponent,
+    ProblemPyramidPageComponent,
+    SchedulePageComponent,
+    TopNavBarComponent
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     FormsModule,
     HttpModule
   ],
